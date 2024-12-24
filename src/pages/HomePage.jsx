a3d3d3df090283a0e101e72ff059e67d3e3c7300
Dhritiman1511 <128951205+Dhritiman1511@users.noreply.github.com>
2024-12-24 20:58:45 +0530

    changes dr-24

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import HeroSection from '../components/HeroSection'
+import About from '../components/About'
+import Achievements from '../components/Achievement'
+import CoursesSection from '../components/CoursesSection'
+
+const HomePage = () => {
+  return (
+    <div>
+        <HeroSection/>
+        <About/>
+        <Achievements/>
+        <CoursesSection/>
+      
+    </div>
+  )
+}
+
+export default HomePage
